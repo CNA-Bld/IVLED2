3ac7a7d3e5e79bc0fc2bf001b52fbf100369fc4e
CNA-Bld <cna.bld@gmail.com>
2015-04-09 19:56:17 +0800

    implement gd driver

--- drivers.py
+++ drivers.py
@@ -1,5 +1,6 @@
 import dropbox
 import httplib2
+from io import BytesIO
 from oauth2client import client
 import apiclient
 from api import ivle
@@ -65,7 +66,7 @@ class DropboxDriver(BaseDriver):
     @classmethod
     def transport_file(cls, user_settings, file_url, target_path):
         if not cls.check_settings(user_settings):
-            return  # TODO
+            return  # Should never reach
         try:
             dropbox_client = dropbox.client.DropboxClient(user_settings['token'])
             file_data = dropbox_client.put_file(user_settings['folder'] + target_path, ivle.get_file(file_url),
@@ -89,17 +90,74 @@ class DropboxDriver(BaseDriver):
 class GoogleDriver(BaseDriver):
     @classmethod
     def check_settings(cls, user_settings):
-        return True
+        if not user_settings['credentials']:
+            raise SyncException("You are not logged in to Google Drive or your token is expired. Please re-login on the webpage.", retry=True, send_email=True,
+                                disable_user=True, logout_user=True)
+        if not user_settings['parent_id']:
+            raise SyncException("You have not set your target folder.", retry=True, send_email=True, disable_user=True, logout_user=False)
+        try:
+            service = cls.get_drive_client(user_settings)
+            if cls.get_folder_name(service, user_settings['parent_id']):
+                return True
+        except client.AccessTokenRefreshError as e:
+            raise SyncException("You are not logged in to Google Drive or your token is expired. Please re-login on the webpage.", retry=True, send_email=True,
+                                disable_user=True, logout_user=True)
+        except apiclient.errors.HttpError as e:
+            raise SyncException(str(e), retry=True, send_email=False, disable_user=False, logout_user=False)
+        except Exception as e:
+            raise SyncException("Something might go wrong with your Google Drive settings. If you are not able to find the error, please inform the developer.",
+                                retry=True, send_email=True, disable_user=True, logout_user=False)
 
     @classmethod
     def transport_file(cls, user_settings, file_url, target_path):
-        return True
+        if not cls.check_settings(user_settings):
+            return  # Should never reach
+        try:
+            service = cls.get_drive_client(user_settings)
+            path_id = cls.find_path(service, user_settings['parent_id'], target_path.split('/')[1:-1])
+            media_body = apiclient.http.MediaIoBaseUpload(BytesIO(ivle.get_file(file_url)), mimetype="application/octet-stream", resumable=True)
+            body = {'title': target_path[target_path.rfind('/') + 1:], 'parents': [{'id': path_id}]}
+            file = service.files().insert(body=body, media_body=media_body).execute()
+            return bool(file['id'])
+        except client.AccessTokenRefreshError as e:
+            raise SyncException("You are not logged in to Google Drive or your token is expired. Please re-login on the webpage.", retry=True, send_email=True,
+                                disable_user=True, logout_user=True)
+        except apiclient.errors.HttpError as e:
+            raise SyncException(str(e), retry=True, send_email=False, disable_user=False, logout_user=False)
+        except Exception as e:
+            raise SyncException("Something might go wrong with your Google Drive settings. If you are not able to find the error, please inform the developer.",
+                                retry=True, send_email=True, disable_user=True, logout_user=False)
+
+    @classmethod
+    def find_path(cls, service, base_path_id, path):
+        if not path:
+            return base_path_id
+        page_token = None
+        while True:
+            param = {}
+            if page_token:
+                param['pageToken'] = page_token
+            children = service.children().list(folderId=base_path_id, **param).execute()
+            for child in children.get('items', []):
+                file = service.files().get(fileId=child['id']).execute()
+                if file['mimeType'] == "application/vnd.google-apps.folder" and file['title'] == path[0]:
+                    return cls.find_path(service, child['id'], path[1:])
+            page_token = children.get('nextPageToken')
+            if not page_token:
+                body = {
+                    'title': path[0],
+                    "parents": [{"id": base_path_id}],
+                    "mimeType": "application/vnd.google-apps.folder",
+                }
+                file = service.files().insert(body=body).execute()
+                return cls.find_path(service, file['id'], path[1:])
+
 
     @classmethod
     def get_drive_client(cls, user_settings):
         if not user_settings['credentials']:
             raise SyncException("You are not logged in to Google Drive or your token is expired. Please re-login on the webpage.", retry=True, send_email=True,
-                                    disable_user=True, logout_user=True)
+                                disable_user=True, logout_user=True)
         credentials = client.OAuth2Credentials.from_json(user_settings['credentials'])
         http_auth = credentials.authorize(httplib2.Http())
         return apiclient.discovery.build('drive', 'v2', http=http_auth)
@@ -110,5 +168,5 @@ class GoogleDriver(BaseDriver):
         return file['title']
 
 
-drivers = {'dropbox': DropboxDriver, '': NullDriver, None: NullDriver}
+drivers = {'dropbox': DropboxDriver, 'google': GoogleDriver, '': NullDriver, None: NullDriver}
 
